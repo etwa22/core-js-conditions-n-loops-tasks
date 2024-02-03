7d224442209a19f2b7a8b0a1216ee088e1e754c1
etwa22 <onadeina1@mail.ru>
2024-02-03 16:00:57 +0300

    feat: add isPositive and getMaxNumber solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,13 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let result = 0;
+  if (a > b && a > c) result = a;
+  else if (b > a && b > c) result = b;
+  else result = c;
+
+  return result;
 }
 
 /**
