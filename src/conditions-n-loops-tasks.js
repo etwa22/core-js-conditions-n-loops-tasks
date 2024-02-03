47605881f17d1f81e5e9d395ddf3b11bc2c6a276
etwa22 <onadeina1@mail.ru>
2024-02-03 16:26:36 +0300

    feat: add isIsoscelesTriangle and canQueenCaptureKing solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -65,8 +65,14 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (
+    queen.x === king.x ||
+    queen.y === king.y ||
+    Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y)
+  )
+    return true;
+  return false;
 }
 
 /**
@@ -87,8 +93,11 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === b && b !== c && a + b > c && a !== 0 && c !== 0) return true;
+  if (a === c && b !== a && a + c > b && a !== 0 && b !== 0) return true;
+  if (b === c && c !== a && b + c > a && b !== 0 && a !== 0) return true;
+  return false;
 }
 
 /**
